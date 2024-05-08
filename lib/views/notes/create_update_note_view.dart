f0f277813f363883ca19953824bfc8bc9d66e1a5
Hackerboi1301 <118775926+Hackerboi1301@users.noreply.github.com>
2024-05-08 16:43:32 -0700

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -2,10 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:notetaker_practiceapp/services/auth/auth_service.dart';
 import 'package:notetaker_practiceapp/services/crud/notes_service.dart';
+import 'package:notetaker_practiceapp/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:notetaker_practiceapp/utilities/generics/get_arguments.dart';
 import 'package:notetaker_practiceapp/services/cloud/cloud_note.dart';
 import 'package:notetaker_practiceapp/services/cloud/cloud_storage_exceptions.dart';
 import 'package:notetaker_practiceapp/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
 
 
 
@@ -97,6 +99,17 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
     appBar: AppBar(
       title: const Text('New Note'),
+      actions: [
+       IconButton(onPressed:() async{
+        final text = _textController.text;
+        if(_note == null || text.isEmpty){
+          await showCannotShareEmptyNoteDialog(context);
+        } else{
+          Share.share(text);
+        }
+
+       }, icon: const Icon(Icons.share)) 
+      ],
     ),
     body: FutureBuilder(
       future: createOrGetExistingNote(context),
